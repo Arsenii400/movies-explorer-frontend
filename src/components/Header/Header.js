d20dcc330f24d98fa0a0f7c68ffb81d39a2d65b2
Arsenii Machekhin <arsenium2@gmail.com>
2022-11-30 02:33:50 +0300

    feat: Сверстал адаптив

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,23 +12,68 @@ function Header() {
       {(pathname === ("/movies") || pathname === ("/saved-movies") || pathname === ("/profile")) &&
         (
           <header className="header header_color_white">
-            <Link exact to="/" className="header__link header__link_type_logo" title="Главная страница">
-              <img className="header__logo" src={logo} alt="#" />
-            </Link>
+
+            <input className="burger burger__checkbox" type="checkbox" id="burger" />
+            <label className="burger burger__label" for="burger" />
+
+            <div className="burger__menu">
+              <nav className="burger__nav">
+                <ul className="burger__list">
+                  <li className="burger__item">
+                    <Link exact to="/" className="burger__link" title="Главная страница">Главная</Link>
+                  </li>
+                  <li className="burger__item">
+                    <NavLink to="/movies" activeClassName="burger__navigation_active" className="burger__link">Фильмы</NavLink>
+                  </li>
+                  <li className="burger__item">
+                    <NavLink to="/saved-movies" activeClassName="burger__navigation_active" className="burger__link">Сохранённые фильмы</NavLink>
+                  </li>
+                  <li className="burger__item">
+                    <Link to="/profile" className="burger__account" title="Ссылка на страницу профиль пользователя">
+                      <p className="burger__account burger__account_type_text">Аккаунт</p>
+                      <div className="burger__account burger__account_type_photo">
+                        <img src={icon} alt="пиктограмка пользователя" />
+                      </div>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
 
             <nav className="header__navigation">
-              <NavLink to="/movies" activeClassName="header__navigation_active" className="header__navLink"
-                title="Выбери киношку">Фильмы</NavLink>
-              <NavLink to="/saved-movies" activeClassName="header__navigation_active" className="header__navLink"
-                title="Вспомни что понравилось">Сохранённые фильмы</NavLink>
+              <ul className="nav__list">
+                <li className="nav__item">
+                  <Link exact to="/" className="nav__link nav__link_type_logo" title="Главная страница">
+                    <img className="nav__logo" src={logo} alt="#" />
+                  </Link>
+                </li>
+                <li className="nav__item">
+                  <ul className="nav__sub-items">
+                    <li className="nav__sub-item">
+                      <NavLink to="/movies" activeClassName="nav__navigation_active" className="nav__navLink"
+                        title="Выбери киношку">
+                        Фильмы
+                      </NavLink>
+                    </li>
+                    <li className="nav__sub-item">
+                      <NavLink to="/saved-movies" activeClassName="nav__navigation_active" className="nav__navLink"
+                        title="Вспомни что понравилось">
+                        Сохранённые фильмы
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav__item">
+                  <Link to="/profile" className="nav__account" title="Ссылка на страницу профиль пользователя">
+                    <p className="nav__account nav__account_type_text">Аккаунт</p>
+                    <div className="nav__account nav__account_type_photo">
+                      <img src={icon} alt="пиктограмка пользователя" />
+                    </div>
+                  </Link>
+                </li>
+              </ul>
             </nav>
 
-            <Link to="/profile" className="header__account" title="Ссылка на страницу профиль пользователя">
-              <p className="header__account header__account_type_text">Аккаунт</p>
-              <div className="header__account header__account_type_photo">
-                <img src={icon} alt="пиктограмка пользователя" />
-              </div>
-            </Link>
           </header>
         )
       }
@@ -37,7 +82,7 @@ function Header() {
         (
           <header className="header header_color_green">
             <Link exact to="/" className="header__link header__link_type_logo" title="Главная страница">
-              <img className="header__logo" src={logo} alt="#" />
+              <img className="header__logo" src={logo} alt="logo" />
             </Link>
 
             <div className="header__wrapper">
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,17 +7,19 @@ function Login() {
   return (
     <section className="login">
       <div className="login__wrapper">
-        <img className="login__logo" src={logo} alt="logo" />
+        <Link exact to="/" title="Главная страница">
+          <img className="login__logo" src={logo} alt="logo" />
+        </Link>
         <p className="login__hello">Рады видеть!</p>
       </div>
       <form className="login__form">
         <label className="login__label" for="email">E-mail</label>
-        <input className="login__input" id="email" type="email" />
+        <input className="login__input" id="email" type="email" required />
         <span className="login__input-error" />
         <label className="login__label" for="password">Пароль</label>
-        <input className="login__input" id="password" type="password" />
+        <input className="login__input" id="password" type="password" minLength="2" maxLength="12" required />
         <span className="login__input-error" />
-        <button className="login__button" type="button">Войти</button>
+        <button className="login__button" type="submit">Войти</button>
       </form>
       <div className="login__linkWrap">
         <p className="login__text">Ещё не зарегистрированы?</p>
